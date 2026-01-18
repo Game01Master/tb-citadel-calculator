6bb302109f0f85fc8e55110d709a83191523c7f7
Game31Master <game31master@gmail.com>
2026-01-18 22:44:11 +0100

    Add GitHub Pages deploy

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
+// IMPORTANT: replace with your repo name
 export default defineConfig({
   plugins: [react()],
-  base: '/tb-citadel-calculator/',
-})
+  base: "/tb-citadel-calculator/",
+});
